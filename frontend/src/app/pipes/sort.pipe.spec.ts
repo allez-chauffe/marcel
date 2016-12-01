a9c3e30ed06b9540cfc60654938b93208e5735c3
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2016-12-01 13:34:55 +0100

    Add sort pipe

---
+++ frontend/src/app/pipes/sort.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SortPipe } from './sort.pipe';
+
+describe('Pipe: Sort', () => {
+  it('create an instance', () => {
+    let pipe = new SortPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
