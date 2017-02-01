bd6fc8bf852b756af32dde5aa9668a0a92795a69
Loyer Aurélien <aurelien.loyer@zenika.com>
2017-02-01 13:53:21 +0100

    edit env prod use rpi ip

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  weatherUrl: 'http://localhost:8090/api/v1/weather/forecast/5',
+  weatherUrl: 'http://10.0.10.63:8090/api/v1/weather/forecast/5',
   weatherKey : 'YOUR_FREE_OPENWEATHER_API_KEY',
 
-  agendaUrl : 'http://localhost:8090/api/v1/agenda/incoming/5'
+  agendaUrl : 'http://10.0.10.63:8090/api/v1/agenda/incoming/5'
 };
