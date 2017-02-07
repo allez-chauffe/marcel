28c1e94914a68e55c01d404ade0cc06610232315
a-cordier <antoine.cordier@zenika.com>
2017-02-07 14:31:18 +0100

    twitter service frontend component

--- frontend/src/app/twitter/twitter.component.ts
+++ frontend/src/app/twitter/twitter.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { TwitterService } from './twitter.service';
+import { Tweet } from './tweet';
 
 @Component({
-  selector: 'app-twitter',
+  selector: 'marcel-twitter',
+  encapsulation: ViewEncapsulation.None,
   templateUrl: './twitter.component.html',
   styleUrls: ['./twitter.component.scss']
 })
 export class TwitterComponent implements OnInit {
 
-  constructor() { }
+  private tweets: Tweet[];
+  private timer: number = 1000 * 60 * 60;
+
+  constructor(private twitterService: TwitterService) { }
 
   ngOnInit() {
+    this.getTimeline();
+    setInterval(this.getTimeline, this.timer);
   }
+
+  getTimeline() {
+    this.twitterService.getTimeline(1).subscribe(tweets => this.tweets = tweets);
+   }
 
 }
--- frontend/src/app/twitter/twitter.service.ts
+++ frontend/src/app/twitter/twitter.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { Tweet } from './tweet';
 
 @Injectable()
 export class TwitterService {
 
-  constructor() { }
+  private apiUrl: string = 'http://localhost:8090/api/v1/twitter/timeline';
 
+  constructor(private http: Http) { }
+
+  getTimeline(nbEvents: number){
+    return this.http.get([this.apiUrl, nbEvents].join('/'))
+      .map(res => <Tweet[]>res.json());
+  }
 }
