a37996c310ade6074bb35ab15285889290ace2d4
Valentin Cocaud <v.cocaud@gmail.com>
2017-05-12 11:35:02 +0200

    Dashboard project initialisation (#52)
    * Deleted frontend directory and added temporary backend
    
    * Added simple polymer front with hello component

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-
-  weatherUrl: 'http://10.0.10.63:8090/api/v1/weather/forecast/5',
-  weatherKey : 'YOUR_FREE_OPENWEATHER_API_KEY',
-
-  agendaUrl : 'http://10.0.10.63:8090/api/v1/agenda/incoming/50'
-};
---
+++ tmpback/app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+let express = require('express');
+let app = express();
+let http = require('http').Server(app);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    next()
+})
+
+app.use(express.static('./public/'));
+app.use(express.static('./components/'));
+app.use(express.static('./node_modules/'));
+
+
+
+app.get('/', function (req, res) {
+    res.sendfile("public/index.html");
+});
+
+/**
+ * Server itself
+ * @type {http.Server}
+ */
+let server = app.listen(8080, function () {
+    //print few information about the server
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Server running and listening @ " + host + ":" + port);
+});
+
+/** list of components to be loaded */
+let componentsList = [
+    {
+        "componentName": "hello",
+        "eltName": "hello-item",
+        "files": "hello.html",
+        "propValues": {
+            "title": "Hello"
+        }
+    }
+];
+
+/**
+ * Get a list of JSON for all registered components
+ * @path /componentsList
+ * @HTTPMethod GET
+ * @returns {string}
+ */
+app.get("/componentsList", function (req, res) {
+    res.send(componentsList);
+});
