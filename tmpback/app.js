49087e2dba52081ac7df69f359666bad3eac265a
Sehsyha <valentin.stern@telecomnancy.net>
2017-05-15 11:16:43 +0200

    Added vlille plugin implementation

--- tmpback/app.js
+++ tmpback/app.js
@@ -34,11 +34,17 @@ let server = app.listen(8080, function () {
 /** list of components to be loaded */
 let componentsList = [
     {
-        "componentName": "hello",
-        "eltName": "hello-item",
-        "files": "hello.html",
+        "componentName": "vlille",
+        "eltName": "vlille-item",
+        "files": "vlille.html",
         "propValues": {
-            "title": "Hello"
+            "stations_id": [
+                { name: "Rihour", id: 10},
+                { name: "Cormontaigne", id: 36 },
+                { name: "Mairie de Lille", id: 64 },
+                { name: "Gare Lille Flandres", id: 25 },
+                { name: "Boulevard Louis XIV", id: 47 }
+            ]
         }
     }
 ];
