d24afb0691217a31da45f8a4a5ea107a5b5ab1df
Sehsyha <valentin.stern@zenika.com>
2017-05-19 15:44:02 +0200

    Video search is now controlled with the voice

--- tmpback/app.js
+++ tmpback/app.js
@@ -7,25 +7,28 @@ const snowboy = require('snowboy');
 const Models = snowboy.Models;
 const Detector = snowboy.Detector;
 const http = require('http').Server(app);
-
+const ApiAi = require('apiai-promise')
+const apiaitoken = require('./config.js').apiaitoken;
+const apiai = ApiAi(apiaitoken);
+const sessionId = 'marcel';
 const models = new Models();
 
 models.add({
-    file: 'resources/snowboy.umdl',
-    sensitivity: '0.8',
-    hotwords: 'snowboy'
+  file: 'resources/snowboy.umdl',
+  sensitivity: '0.8',
+  hotwords: 'snowboy'
 })
 
 const detector = new Detector({
-    resource: 'resources/common.res',
-    audioGain: 2.0,
-    models
+  resource: 'resources/common.res',
+  audioGain: 2.0,
+  models
 })
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-    next()
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
 })
 
 app.use(express.static('./public/'));
@@ -33,7 +36,7 @@ app.use(express.static('./components/'));
 app.use(express.static('./node_modules/'));
 
 app.get('/', (req, res) => {
-    res.sendfile("public/index.html");
+  res.sendfile("public/index.html");
 });
 
 /**
@@ -41,88 +44,92 @@ app.get('/', (req, res) => {
  * @type {http.Server}
  */
 const server = app.listen(8080, () => {
-    //print few information about the server
-    const host = server.address().address;
-    const port = server.address().port;
-    console.log("Server running and listening @ " + host + ":" + port);
+  //print few information about the server
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Server running and listening @ " + host + ":" + port);
 });
 const io = require('socket.io')(server);
 
 io.on('connection', socket => {
-    socket.on('speech', (message) => console.log(message))
-    setTimeout(() => socket.emit('youtube', {"type": "search", "content": "chat"}), 2000);
-    setTimeout(() => socket.emit('youtube', {"type": "pause"}), 4000);
-    setTimeout(() => socket.emit('youtube', {"type": "play"}), 6000);
-    setTimeout(() => socket.emit('youtube', {"type": "stop"}), 8000);
+  socket.on('speech', (speech) => {
+    apiai.textRequest(speech.message, { sessionId })
+         .then((response) => {
+            if (response.result.parameters.video !== undefined) {
+              socket.emit('youtube', {"type": "search", "content": response.result.parameters.video});
+            }
+          })
+          .catch(err => console.log(err));
+  });
 })
 
 detector.on('hotword', (index, hotword) => {
-    console.log('hotword detected');
-    io.sockets.emit('hotword');
+  console.log('hotword detected');
+  io.sockets.emit('hotword');
 })
 
 /** list of components to be loaded */
 const componentsList = {
-    "styles": [
-        "css/style.css",
-        "css/font-awesome.min.css",
-        "css/weather-icons.min.css"
-    ],
-    "scripts": [
-        "socket.io/socket.io.js",
-        "js/connect-socketio.js"
-    ],
-    "components": [
-        {
-            "componentName": "youtube",
-            "eltName": "youtube-item",
-            "files": "youtube.html"
-        },
-        {
-            "componentName": "marcel",
-            "eltName": "marcel-item",
-            "files": "marcel.html",
-            "propValues": {
-                "logo_url": "assets/zenika.png",
-                "message_text1": "Bienvenue",
-                "message_text2": "à Zenika Lille",
-                "github_users": [
-                    'Gillespie59',
-                    'GwennaelBuchet',
-                    'T3kstiil3',
-                    'RemiEven',
-                    'looztra',
-                    'a-cordier',
-                    'wadendo',
-                    'NathanDM',
-                    'Antoinephi',
-                    'cluster',
-                    'yyekhlef',
-                    'gdrouet',
-                    'Kize',
-                    'kratisto',
-                    'Sehsyha',
-                    'P0ppoff'
-                ],
-                "github_client_id": "27b62f039b44ddc08fdf",
-                "github_client_secret": "7b14f465112e87267a72c02d4c3fc58925412dbd",
-                "twitter_api": "http://10.0.10.63:8090/api/v1/twitter/timeline",
-                "vlille_stations_id": [
-                    { name: "Rihour", id: 10},
-                    { name: "Cormontaigne", id: 36 },
-                    { name: "Mairie de Lille", id: 64 },
-                    { name: "Gare Lille Flandres", id: 25 },
-                    { name: "Boulevard Louis XIV", id: 47 }
-                ],
-                "soundtouch_url": "http://10.0.10.166:8090/now_playing",
-                "weather_api_key": "FREE_OPENWEATHER_KEY",
-                "weather_city": "Lille,Fr",
-                "weather_url": "http://10.0.10.63:8090/api/v1/weather/forecast/5",
-                "calendar_url": "http://10.0.10.63:8090/api/v1/agenda/incoming/50?json_callback=JSON_CALLBACK",
-                "speech_default_message": "Bonjour à tous, je suis MARCEL !"
-            }
-        }
-    ]
+  "styles": [
+    "css/style.css",
+    "css/font-awesome.min.css",
+    "css/weather-icons.min.css"
+  ],
+  "scripts": [
+    "socket.io/socket.io.js",
+    "js/connect-socketio.js"
+  ],
+  "components": [
+    {
+      "componentName": "youtube",
+      "eltName": "youtube-item",
+      "files": "youtube.html"
+    },
+    {
+      "componentName": "marcel",
+      "eltName": "marcel-item",
+      "files": "marcel.html",
+      "propValues": {
+        "logo_url": "assets/zenika.png",
+        "message_text1": "Bienvenue",
+        "message_text2": "à Zenika Lille",
+        "github_users": [
+          'Gillespie59',
+          'GwennaelBuchet',
+          'T3kstiil3',
+          'RemiEven',
+          'looztra',
+          'a-cordier',
+          'wadendo',
+          'NathanDM',
+          'Antoinephi',
+          'cluster',
+          'yyekhlef',
+          'gdrouet',
+          'Kize',
+          'kratisto',
+          'Sehsyha',
+          'P0ppoff'
+        ],
+        "github_client_id": "27b62f039b44ddc08fdf",
+        "github_client_secret": "7b14f465112e87267a72c02d4c3fc58925412dbd",
+        "twitter_api": "http://10.0.10.63:8090/api/v1/twitter/timeline",
+        "vlille_stations_id": [
+          { name: "Rihour", id: 10 },
+          { name: "Cormontaigne", id: 36 },
+          { name: "Mairie de Lille", id: 64 },
+          { name: "Gare Lille Flandres", id: 25 },
+          { name: "Boulevard Louis XIV", id: 47 }
+        ],
+        "soundtouch_url": "http://10.0.10.166:8090/now_playing",
+        "weather_api_key": "FREE_OPENWEATHER_KEY",
+        "weather_city": "Lille,Fr",
+        "weather_url": "http://10.0.10.63:8090/api/v1/weather/forecast/5",
+        "calendar_url": "http://10.0.10.63:8090/api/v1/agenda/incoming/50?json_callback=JSON_CALLBACK",
+        "speech_default_message": "Bonjour à tous, je suis MARCEL !"
+      }
+    }
+  ]
 };
 
 /**
@@ -132,5 +139,5 @@ const componentsList = {
  * @returns {string}
  */
 app.get("/componentsList", function (req, res) {
-    res.send(componentsList);
+  res.send(componentsList);
 });
