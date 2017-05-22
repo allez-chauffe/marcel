931ae17b37fbd29e3665037dd637e318683fc8ae
Valentin Cocaud <v.cocaud@gmail.com>
2017-05-22 10:57:43 +0200

    :sparkles: Created searchable plugins list

--- back-office/src/MainScreen/MainScreen.jsx
+++ back-office/src/MainScreen/MainScreen.jsx
@@ -1,12 +1,20 @@
 // @flow
 import React from 'react'
-
+import { range } from 'lodash'
+import { PluginList } from '../plugins'
 import './MainScreen.css'
+
+//TODO Remove mocked data
+const availablePlugins = range(20).map(i => ({
+  name: `Plugin ${i}`,
+  elementName: `plugin-${i}`,
+  icon: 'picture_in_picture_alt',
+}))
 
 export const MainScreen = () => (
   <div className="MainScreen">
     <div className="left-side-panel">
-      left-side-panel
+      <PluginList plugins={availablePlugins} />
     </div>
     <div className="main-panel">
       main-panel
---
+++ back-office/src/plugins/PluginList/PluginList.jsx
@@ -0,0 +1,55 @@
+// @flow
+import React from 'react'
+import List from 'react-toolbox/lib/list/List'
+import ListItem from 'react-toolbox/lib/list/ListItem'
+import Input from 'react-toolbox/lib/input/Input'
+import { chain } from 'lodash'
+
+type Plugin = {
+  name: string,
+  elementName: string,
+  icon: string,
+}
+
+export class PluginList extends React.Component {
+  props: { plugins: Plugin[] }
+  state = { filter: '', regexp: /.*/ }
+
+  onSearchChange = (filter: string) => {
+    //Remove spaces and add 'any char' matcher between each chars.
+    const regexPatern = chain(filter).split('').without(' ').join('.*').value()
+    const regexp = RegExp(`.*${regexPatern}.*`, 'i')
+    this.setState({ regexp, filter })
+  }
+
+  render() {
+    const { plugins } = this.props
+    const { filter, regexp } = this.state
+
+    const pluginNodes = chain(plugins)
+      .filter(({ name }) => regexp.test(name))
+      .map(plugin => (
+        <ListItem
+          caption={plugin.name}
+          key={plugin.elementName}
+          legend={plugin.elementName}
+          leftIcon={plugin.icon}
+        />
+      ))
+      .value()
+
+    return (
+      <div>
+        <Input
+          label="Search plugin"
+          icon="search"
+          value={filter}
+          onChange={this.onSearchChange}
+        />
+        <List selectable>
+          {pluginNodes}
+        </List>
+      </div>
+    )
+  }
+}
