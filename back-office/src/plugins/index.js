049e9ae33a5ad2d95895999c30299a75eac43fd7
Valentin Cocaud <v.cocaud@gmail.com>
2017-05-30 11:26:31 +0200

    :sparkles: Add plugin props in Redux

--- back-office/src/plugins/index.js
+++ back-office/src/plugins/index.js
@@ -1,6 +1,6 @@
 // @flow
 export * from './props'
 export { default as PluginList } from './PluginList'
-export * from './plugins.types'
+export * from './plugins.type'
 export { default as reducer } from './reducer'
 export * from './actions'
--- back-office/src/plugins/plugins.types.js
+++
@@ -1,8 +0,0 @@
-// @flow
-export type Plugin = {
-  name: string,
-  elementName: string,
-  icon: string,
-}
-
-export type State = Plugin[]
