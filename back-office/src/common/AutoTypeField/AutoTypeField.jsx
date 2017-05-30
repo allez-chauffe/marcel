1a3f5891728b290261f94ccc5210b02a6f7b176d
Valentin Cocaud <v.cocaud@gmail.com>
2017-05-30 10:04:09 +0200

    :pencil2: Fixed Input prop name

--- back-office/src/common/AutoTypeField/AutoTypeField.jsx
+++ back-office/src/common/AutoTypeField/AutoTypeField.jsx
@@ -8,12 +8,12 @@ const AutoTypeField = ({ value }: { value: Prop }) => {
   if (value.type === 'string')
     return <Input value={value.value} name={value.name} />
   if (value.type === 'number')
-    return <Input type="number" prop={value.value} name={value.name} />
+    return <Input type="number" value={value.value} name={value.name} />
   if (value.type === 'boolean')
     return <Switch checked={value.value} name={value.name} />
   if (value.type === 'json') {
     const stringValue = JSON.stringify(value.value, null, 2)
-    return <Input multiline={true} value={stringValue} />
+    return <Input multiline={true} value={stringValue} name={value.name} />
   }
 
   return <Input value={value.value} />
