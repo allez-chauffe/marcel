2ae5492ccf7046960e337ccc6aff024216789b33
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-06-02 17:36:52 +0200

    [Improve] Load medias configuration from memory

--- backend/media/mediaservice.go
+++ backend/media/mediaservice.go
@@ -5,22 +5,53 @@ import (
 	"github.com/gorilla/mux"
 	"strconv"
 	"io/ioutil"
+	"encoding/json"
+	"errors"
 )
 
-func GetMedia(w http.ResponseWriter, r *http.Request) {
+var Medias []Media
+
+func LoadMedias() {
+	content, err := ioutil.ReadFile("data/media.config.json")
+	check(err)
+
+	err = json.Unmarshal(content, &Medias)
+	check(err)
+}
+
+func GetMedia(idMedia int) (*Media, error) {
+	for _, m := range Medias {
+		if idMedia == m.ID {
+			return &m, nil
+		}
+	}
+
+	return nil, errors.New("NO_MEDIA_FOUND")
+}
+
+func HandleGetMedia(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	f := vars["idMedia"]
 	idMedia, _ := strconv.Atoi(f)
 
-	if idMedia > 0 {
-
-		config, err := ioutil.ReadFile("data/media.config.json")
-		check(err)
-
-		if err == nil {
-			w.Write([]byte(config))
-		}
+	m, err := GetMedia(idMedia)
+	if err != nil {
+		writeResponseWithError(w, http.StatusNotFound)
+		return
 	}
+
+	b, err := json.Marshal(*m)
+	if err != nil {
+		writeResponseWithError(w, http.StatusNotFound)
+		return
+	}
+
+	w.Write([]byte(b))
+
+}
+
+func writeResponseWithError(w http.ResponseWriter, errorCode int) {
+	w.WriteHeader(errorCode)
 }
 
 func check(e error) {
