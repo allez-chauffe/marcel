cb89892a0fbe4e8a6470d7b2f78af9f98a5d1f55
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-06-06 19:05:12 +0200

    [Add] Description's comments

--- backend/media/media.go
+++ backend/media/media.go
@@ -1,23 +1,32 @@
 package media
 
+/**
+The global attributes for a Media
+ */
 type Media struct {
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	Config  MediaConfig `json:"config"`
-	Plugins []MediaPlugins `json:"plugins"`
+	Plugins []MediaPlugin `json:"plugins"`
 }
 
 type MediaConfig struct {
 	Styles []string `json:"styles"`
 }
 
-type MediaPlugins struct {
+/**
+Properties and configuration for a plugin used in the media
+ */
+type MediaPlugin struct {
 	Name       string `json:"name"`
 	EltName    string `json:"eltName"`
 	Files      []string `json:"files"`
 	PropValues MediaPluginProps `json:"propValues"`
 }
 
+/**
+Because we don't know what will compounds the props for a plugin, we use a map[string] interface{}
+ */
 type MediaPluginProps struct {
 	X map[string]interface{} `json:"-"`
 }
