b8b764c6ea37660a68380b9063da98ea7235ac41
Valentin Cocaud <v.cocaud@gmail.com>
2017-06-13 18:19:21 +0200

    :sparkles: Add plugin addtion to existing dashboard

--- back-office/src/plugins/PluginList/PluginList.jsx
+++ back-office/src/plugins/PluginList/PluginList.jsx
@@ -2,31 +2,25 @@
 import React from 'react'
 import List from 'react-toolbox/lib/list/List'
 import ListItem from 'react-toolbox/lib/list/ListItem'
-import { chain } from 'lodash'
+import IconButton from 'react-toolbox/lib/button/IconButton'
 
 import { SearchField } from '../../common'
 import type { Plugin } from '../type'
 
+const AddButton = ({ onClick }) => (
+  <IconButton icon="add" primary onClick={onClick} />
+)
+
 export default class PluginList extends React.Component {
-  props: { plugins: Plugin[], filter: string, changeFilter: string => void }
+  props: {
+    plugins: Plugin[],
+    filter: string,
+    changeFilter: string => void,
+    addPlugin: Plugin => void,
+  }
 
   render() {
-    const { plugins, filter, changeFilter } = this.props
-
-    const regexPatern = chain(filter).split('').without(' ').join('.*').value()
-    const regexp = RegExp(`.*${regexPatern}.*`, 'i')
-
-    const pluginNodes = chain(plugins)
-      .filter(({ name }) => regexp.test(name))
-      .map(plugin => (
-        <ListItem
-          caption={plugin.name}
-          key={plugin.elementName}
-          legend={plugin.elementName}
-          leftIcon={plugin.icon}
-        />
-      ))
-      .value()
+    const { plugins, filter, changeFilter, addPlugin } = this.props
 
     return (
       <div>
@@ -36,7 +30,19 @@ export default class PluginList extends React.Component {
           onChange={changeFilter}
         />
         <List selectable>
-          {pluginNodes}
+          {plugins.map(plugin => {
+            const { name, elementName, icon } = plugin
+            return (
+              <ListItem
+                caption={name}
+                ripple={false}
+                key={elementName}
+                legend={elementName}
+                leftIcon={icon}
+                rightIcon={<AddButton onClick={() => addPlugin(plugin)} />}
+              />
+            )
+          })}
         </List>
       </div>
     )
