810583351e69fadbed5ef80a83559b2af1dbb58f
Valentin Cocaud <v.cocaud@gmail.com>
2017-06-14 16:13:57 +0200

    :art: Add State type to PluginList container

--- back-office/src/plugins/PluginList/PluginList.container.js
+++ back-office/src/plugins/PluginList/PluginList.container.js
@@ -2,13 +2,14 @@
 import { connect } from 'react-redux'
 import PluginList from './PluginList'
 import { addPlugin } from '../../dashboard'
+import type { State } from '../../store'
 import {
   pluginFilterSelector,
   changePluginFilter,
   filteredPluginsSeletor,
 } from '../../store/filters'
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: State) => ({
   filter: pluginFilterSelector(state),
   plugins: filteredPluginsSeletor(state),
 })
