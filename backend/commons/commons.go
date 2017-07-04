defb2d45699c92a44e2b5e73c831f0dfa06290fe
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-07-04 17:07:36 +0200

    [Add] Common functions to respond with error

--- backend/commons/commons.go
+++ backend/commons/commons.go
@@ -4,6 +4,8 @@ import (
 	"math/rand"
 	"strconv"
 	"time"
+	"net/http"
+	"encoding/json"
 )
 
 func init() {
@@ -27,4 +29,16 @@ func randomString(l int) string {
 
 func randInt(min int, max int) int {
 	return min + rand.Intn(max-min)
+}
+
+func respondWithError(w http.ResponseWriter, code int, message string) {
+	respondWithJSON(w, code, map[string]string{"error": message})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	response, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(response)
 }
