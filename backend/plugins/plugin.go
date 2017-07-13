94cacdfecf9274c34176579b47794a93f33c7204
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-07-13 12:19:23 +0200

    [Add] first shape of a plugin in the catalog

--- backend/plugins/plugin.go
+++ backend/plugins/plugin.go
@@ -1,15 +1,57 @@
 package plugins
 
+// PluginsConfiguration encapsulates all configuration data
+//
+// swagger:model
+type Configuration struct {
+	LastID  int     `json:"lastid"`
+	Plugins []Plugin `json:"plugins"`
+}
+
+func NewConfiguration() *Configuration {
+	var configuration = new(Configuration)
+
+	configuration.LastID = 0
+	configuration.Plugins = []Plugin{}
+
+	return configuration
+}
+
 // Plugin represents a plugin configuration
 //
 // swagger:model
 type Plugin struct {
-	ID     int           `json:"id"`
-	Name   string        `json:"title"`
-	Params []PluginParam `json:"params"`
+	ID          int    `json:"id"`
+	Name        string        `json:"title"`
+	Description string    `json:"description"`
+	Frontend    []Frontend `json:"frontend"`
+	Backend     []Backend `json:"backend"`
 }
 
-type PluginParam struct {
+func NewPlugin() (*Plugin) {
+	var p = new(Plugin)
+
+	return p
+}
+
+type Frontend struct {
 	Key   string
 	Value string
+}
+
+func NewFrontend() *Frontend {
+	var p = new(Frontend)
+
+	return p
+}
+
+type Backend struct {
+	Key   string
+	Value string
+}
+
+func NewBackend() *Backend {
+	var p = new(Backend)
+
+	return p
 }
