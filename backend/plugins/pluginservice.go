83c43f5389f132f5dd2d999f7b197d7be21358a0
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-07-13 12:21:03 +0200

    [Add] plugin service (to be improved by inherit from media service)

---
+++ backend/plugins/pluginservice.go
@@ -0,0 +1,69 @@
+package plugins
+
+import (
+	"net/http"
+	"encoding/json"
+	"github.com/Zenika/MARCEL/backend/commons"
+)
+
+const PLUGINS_CONFIG_PATH string = "data"
+const PLUGINS_CONFIG_FILENAME string = "plugins.config.json"
+
+type Service struct {
+	Manager *Manager
+}
+
+func NewService() *Service {
+	var p = new(Service)
+
+	c := NewConfiguration()
+	p.Manager = NewManager(PLUGINS_CONFIG_PATH, PLUGINS_CONFIG_FILENAME, c)
+
+	return p
+}
+
+
+func (s *Service) GetManager() (*Manager) {
+	return s.Manager
+}
+
+
+// swagger:route GET /plugins/config GetConfigHandler
+//
+// Gets information of all plugins
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+func (m *Service) GetConfigHandler(w http.ResponseWriter, r *http.Request) {
+
+	c := m.Manager.GetConfiguration()
+	b, err := json.Marshal(c)
+	if err != nil {
+		commons.WriteResponse(w, http.StatusNotFound)
+		return
+	}
+
+	w.Write([]byte(b))
+}
+
+// swagger:route GET /plugins GetAllHandler
+//
+// Gets information of all plugins
+//
+//     Produces:
+//     - application/json
+//
+//     Schemes: http, https
+func (m *Service) GetAllHandler(w http.ResponseWriter, r *http.Request) {
+
+	media := m.Manager.GetAll()
+	b, err := json.Marshal(media)
+	if err != nil {
+		commons.WriteResponse(w, http.StatusNotFound)
+		return
+	}
+
+	w.Write([]byte(b))
+}
