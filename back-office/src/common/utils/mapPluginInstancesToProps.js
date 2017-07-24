d3156c0fc0b3a4c8db5aeb81f5995d3ab9ec75c3
Valentin Cocaud <v.cocaud@gmail.com>
2017-07-24 18:00:37 +0200

    :hammer: Refactor mapPluginInstancesToProps with immutadot

--- back-office/src/common/utils/mapPluginInstancesToProps.js
+++ back-office/src/common/utils/mapPluginInstancesToProps.js
@@ -1,25 +1,19 @@
 //@flow
-import { pickBy, merge, omitBy, mapValues } from 'lodash'
+import { pickBy, merge, omitBy } from 'lodash'
+import { map, mapValues } from 'immutadot'
 import type { PluginInstanceMap, PluginInstance } from '../../dashboard'
 
 const mapPluginInstancesToProps = (pluginInstances: PluginInstanceMap) => (
   instanceId: string,
 ): PluginInstance => {
   const getPluginInstance = mapPluginInstancesToProps(pluginInstances)
-  const pluginInstance = pluginInstances[instanceId]
-  if (!pluginInstance) throw new Error('Plugin instance not found')
 
-  const { props, ...otherAttributes } = pluginInstance
-  return {
-    ...otherAttributes,
-    props: merge(
-      omitBy(props, { type: 'pluginList' }),
-      mapValues(pickBy(props, { type: 'pluginList' }), prop => ({
-        ...prop,
-        value: prop.value.map(getPluginInstance),
-      })),
-    ),
-  }
+  return mapValues(
+    pluginInstances[instanceId],
+    `props`,
+    prop =>
+      prop.type === 'pluginList' ? map(prop, `value`, getPluginInstance) : prop,
+  )
 }
 
 export default mapPluginInstancesToProps
