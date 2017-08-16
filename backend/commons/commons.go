8cdf5db72488152d374d5afd129af7ffdebb8fd8
Gwennael Buchet <gwennael.buchet@gmail.com>
2017-08-16 16:10:05 +0200

    [Add] CopyFile & CopyDir

--- backend/commons/commons.go
+++ backend/commons/commons.go
@@ -9,6 +9,8 @@ import (
 	"reflect"
 	"strings"
 	"os"
+	"fmt"
+	"io"
 )
 
 func init() {
@@ -64,9 +66,82 @@ func FileBasename(s string) string {
 
 func FileOrFolderExists(path string) (bool, error) {
 	_, err := os.Stat(path)
-	if err == nil { return true, nil }
-	if os.IsNotExist(err) { return false, nil }
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
 	return true, err
+}
+
+// Thx to https://www.socketloop.com/tutorials/golang-copy-directory-including-sub-directories-files
+func CopyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	defer sourcefile.Close()
+
+	destfile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+		}
+
+	}
+
+	return
+}
+
+// Thx to https://www.socketloop.com/tutorials/golang-copy-directory-including-sub-directories-files
+func CopyDir(source string, dest string) (err error) {
+
+	// get properties of source dir
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	// create dest dir
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+
+	directory, _ := os.Open(source)
+
+	objects, err := directory.Readdir(-1)
+
+	for _, obj := range objects {
+		sourcefilepointer := source + "/" + obj.Name()
+		destinationfilepointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			// create sub-directories - recursively
+			err = CopyDir(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			// perform copy
+			err = CopyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	}
+	return
 }
 
 func WriteResponse(w http.ResponseWriter, statusCode int, message string) {
