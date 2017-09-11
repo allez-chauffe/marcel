9a26700e4768c477187f94e41e629d5d7305c43a
Valentin Cocaud <v.cocaud@gmail.com>
2017-09-11 17:35:16 +0200

    :wrench: Update go dependencies

--- scripts/install_go_deps.sh
+++ scripts/install_go_deps.sh
@@ -1,20 +1,10 @@
 echo "Installing go dependencies..."
-echo "    - github.com/briandowns/openweathermap"
-go get github.com/briandowns/openweathermap
-echo "    - github.com/GwennaelBuchet/openweathermap"
-go get github.com/GwennaelBuchet/openweathermap
-echo "    - github.com/dghubble/go-twitter/twitter"
-go get github.com/dghubble/go-twitter/twitter
-echo "    - github.com/dghubble/oauth1"
-go get github.com/dghubble/oauth1
+echo ""
 echo "    - github.com/rs/cors"
 go get github.com/rs/cors
 echo "    - github.com/gorilla/mux"
 go get -u github.com/gorilla/mux
-echo "    - google.golang.org/api/calendar/v3"
-go get -u google.golang.org/api/calendar/v3
-echo "    - golang.org/x/oauth2/..."
-go get -u golang.org/x/oauth2/...
 echo "    - github.com/mitchellh/mapstructure"
 go get github.com/mitchellh/mapstructure
+echo ""
 echo "Go dependencies installation finished"
