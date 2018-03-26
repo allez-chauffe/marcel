0065f3ccb7f6e64ab9f2ae4275c85387dc727a8c
nlepage <19571875+nlepage@users.noreply.github.com>
2018-03-26 14:51:54 +0200

    :bug: Prefix cookie names to avoid subdomains cookie errors fix #313

--- auth-backend/conf/config.go
+++ auth-backend/conf/config.go
@@ -12,7 +12,8 @@ type Config struct {
 	AuthExpiration    int64  `json:"authExpiration"`
 	RefreshExpiration int64  `json:"refreshExpiration"`
 	Domain            string `json:"domain"`
-	BaseURL           string `json:"baseURL`
+	BaseURL           string `json:"baseURL"`
+	CookiesNamePrefix string `json:"cookiesNamePrefix"`
 }
 
 const configPath = "config/config.json"
