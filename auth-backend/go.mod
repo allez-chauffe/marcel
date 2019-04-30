89f6d28937610ccc1c02945c5dc6883e24088bf8
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-04-30 16:25:07 +0200

    :recycle: Backends: Use cobra (#326)
    * :construction: Add cobra
    
    * :construction: Add a flag for listening port
    
    * :recycle: Use cobra in backend
    
    * :art:
    
    * :bulb:
    
    * :recycle: marcel has no args...
    
    * :recycle: Use cobra in auth-backend
    
    * :ok_hand:

--- auth-backend/go.mod
+++ auth-backend/go.mod
@@ -8,6 +8,9 @@ require (
 	github.com/gorilla/context v1.1.1 // indirect
 	github.com/gorilla/handlers v1.3.0
 	github.com/gorilla/mux v1.6.1
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/rs/cors v0.0.0-20170727213201-7af7a1e09ba3
 	github.com/satori/go.uuid v1.2.0
+	github.com/spf13/cobra v0.0.3
+	github.com/spf13/pflag v1.0.3 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,24 +1,13 @@
-//go:generate swagger generate spec -o ./swagger.json
-
-// Package main MARCEL APIs
-//
-// Provide API to access medias information
-//
-//     Host: localhost:8090
-//     BasePath: /api/v1/
-//     Version: 1.1
-//     License: MIT http://opensource.org/licenses/MIT
-//     Contact: Gwennael Buchet<gwennael.buchet@gmail.com>
-//
-// swagger:meta
 package main
 
-import "github.com/Zenika/MARCEL/backend/app"
+import (
+	"log"
+
+	"github.com/Zenika/MARCEL/backend/cmd"
+)
 
 func main() {
-
-	a := new(app.App)
-	a.Initialize()
-
-	a.Run(":8090")
+	if err := cmd.MarcelCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
