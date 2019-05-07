0ecedcb51368179da402712c129a852a0ab5d798
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-05-07 15:24:23 +0200

    :bug: avoid initializing config and logs when not necessary

--- cmd/marcel.go
+++ cmd/marcel.go
@@ -11,26 +11,29 @@ import (
 	"github.com/Zenika/MARCEL/config"
 )
 
+var configFile string
+
 func init() {
 	Marcel.PersistentFlags().Var((*LogLevel)(&config.Config.LogLevel), "logLevel", fmt.Sprintf("Log level: %s, %s, %s, %s or %s", log.TraceLevel, log.DebugLevel, log.InfoLevel, log.WarnLevel, log.ErrorLevel))
 	viper.BindPFlag("logLevel", Marcel.PersistentFlags().Lookup("logLevel"))
 
-	var configFile = Marcel.PersistentFlags().StringP("configFile", "c", "", fmt.Sprintf("Config file (default /etc/marcel/config.xxx or ./config.xxx, supports %s)", strings.Join(viper.SupportedExts, " ")))
-
-	cobra.OnInitialize(
-		func() {
-			config.Init(*configFile)
-		},
-		setLogLevel,
-		debugConfig,
-	)
+	Marcel.PersistentFlags().StringVarP(&configFile, "configFile", "c", "", fmt.Sprintf("Config file (default /etc/marcel/config.xxx or ./config.xxx, supports %s)", strings.Join(viper.SupportedExts, " ")))
 }
 
 // Marcel is the root command of Marcel
 var Marcel = &cobra.Command{
-	Use:   "marcel",
-	Short: "Marcel is a configurable plugin based dashboard system",
-	Args:  cobra.NoArgs,
+	Use:           "marcel",
+	Short:         "Marcel is a configurable plugin based dashboard system",
+	Args:          cobra.NoArgs,
+	SilenceErrors: true,
+	PersistentPreRun: func(cmd *cobra.Command, args []string) {
+		if cmd.Name() == "help" {
+			return
+		}
+		config.Init(configFile)
+		setLogLevel()
+		debugConfig()
+	},
 }
 
 // LogLevel implements a pflag.Value with logrus.Level
