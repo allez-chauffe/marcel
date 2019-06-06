805be5206809c163fe05bec4a415d4151ca862e5
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-06-07 00:34:44 +0200

    :construction: PoC users export

--- api/db/db.go
+++ api/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	log "github.com/sirupsen/logrus"
 	bh "github.com/timshannon/bolthold"
+	bolt "go.etcd.io/bbolt"
 
 	"github.com/Zenika/MARCEL/api/db/internal/db"
 	"github.com/Zenika/MARCEL/api/db/users"
@@ -10,9 +11,26 @@ import (
 )
 
 func Open() error {
+	return open(false)
+}
+
+func OpenRO() error {
+	return open(true)
+}
+
+func open(readOnly bool) error {
+	var options = *bolt.DefaultOptions
+	options.ReadOnly = readOnly
+
 	var err error
-	if db.Store, err = bh.Open(config.Config.DBFile, 0644, nil); err != nil {
+	if db.Store, err = bh.Open(config.Config.DBFile, 0644, &bh.Options{
+		Options: &options,
+	}); err != nil {
 		return err
+	}
+
+	if readOnly {
+		return nil
 	}
 
 	if err := users.EnsureOneUser(); err != nil {
