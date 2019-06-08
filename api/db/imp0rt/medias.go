c680ba1609b2243f004e018f3a842236d284c2ac
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-06-08 18:10:49 +0200

    :bug:

---
+++ api/db/imp0rt/medias.go
@@ -0,0 +1,11 @@
+package imp0rt
+
+import "github.com/Zenika/MARCEL/api/db/medias"
+
+func Medias(inputFile string) error {
+	var mediasList []medias.Media
+
+	return imp0rt(inputFile, &mediasList, func() error {
+		return medias.UpsertAll(mediasList)
+	})
+}
