806860690f1d5e302878377bf532f364e9203141
vpoilvert <vpoilvert@users.noreply.github.com>
2019-06-18 10:35:53 +0200

    :construction_worker: Push docker images only for master and tags (#321)
    * :construction_worker: Push docker images only for release and master branches
    
    * :alembic:
    
    * :alembic:
    
    * :alembic:
    
    * :alembic:
    
    * :alembic:
    
    * :alembic:
    
    * :alembic: Work with tags

---
+++ scripts/docker_build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script used to build docker images on CircleCI
+# $1 : image name (without version)
+set -e
+set -o pipefail
+# Any subsequent(*) commands which fail will cause the shell script to exit immediately
+
+IMAGE_NAME=${1}
+
+PUSH=false
+if [[ "${CIRCLE_BRANCH}" == "master" || "${CIRCLE_TAG}" != "" ]] ; then
+  PUSH=true
+fi
+
+IMAGE_VERSION=dev
+if [[ "${CIRCLE_TAG}" != "" ]] ; then
+  IMAGE_VERSION="$CIRCLE_TAG"
+  PUSH=true
+fi
+
+echo "IMAGE_NAME    = ${IMAGE_NAME}"
+echo "IMAGE_VERSION = ${IMAGE_VERSION}"
+echo "PUSH          = ${PUSH}"
+
+# Build docker image.
+docker build -t ${IMAGE_NAME}:${IMAGE_VERSION} .
+
+# Push image to docker hub.
+if [ "${PUSH}" == "true" ] ; then
+  docker login -u ${DOCKER_LOGIN} -p ${DOCKER_PASSWORD}
+  docker push ${IMAGE_NAME}:${IMAGE_VERSION}
+fi
