c138115f5ae6fb6073e7fd26f7a6e724c8c56f1f
Valentin Cocaud <v.cocaud@gmail.com>
2019-09-06 15:48:20 +0200

    :bug: fix frontend proxy in dev mode

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,6 @@
+const proxy = require('http-proxy-middleware')
+
+module.exports = function (app) {
+  app.use(proxy('/api', { target: 'http://localhost:8090/' }))
+  app.use(proxy('/config', { target: 'http://localhost:8090/' }))
+}
