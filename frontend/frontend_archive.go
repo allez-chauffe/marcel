2095add26ed516b87ffecc9a8d385de90f25b5e1
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-11-12 10:33:03 +0100

    :bug: fix frontends download...

--- frontend/frontend_archive.go
+++ frontend/frontend_archive.go
@@ -13,7 +13,7 @@ import (
 )
 
 func initFs() (http.FileSystem, error) {
-	url := fmt.Sprintf("https://github.com/Zenika/marcel/releases/download/%s/marcel-frontend.tgz", version.Version)
+	url := fmt.Sprintf("https://github.com/Zenika/marcel/releases/download/%s/marcel-frontend.tgz", version.Version())
 	path := filepath.Join(config.Default().API().DataDir(), "frontend", version.Version())
 
 	if err := httputil.DownloadTgzToDir(url, path); err != nil {
