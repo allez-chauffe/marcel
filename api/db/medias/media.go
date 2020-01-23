412c29a0ca662fb38da2cf8b7347c837c311e59f
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2020-01-23 21:46:09 +0100

    :ok_hand: Review model

--- api/db/medias/media.go
+++ api/db/medias/media.go
@@ -2,39 +2,44 @@ package medias
 
 // Media represents a media configuration
 type Media struct {
-	ID          int                    `json:"id"`
-	Short       string                 `json:"short"`
-	Long        string                 `json:"long"`
-	Owner       string                 `json:"owner"`
-	IsActive    bool                   `json:"isactive"`
-	Rows        int                    `json:"rows"`
-	Cols        int                    `json:"cols"`
-	ScreenRatio float64                `json:"screenRatio"` // FIXME rename to ratio?
-	Stylesvar   map[string]interface{} `json:"stylesvar"`
-	DisplayGrid bool                   `json:"displayGrid"`
-	Widgets     []Widget               `json:"widgets"`
+	ID       int               `json:"id"`
+	Short    string            `json:"short"`
+	Long     string            `json:"long"`
+	Owner    string            `json:"owner"`
+	IsActive bool              `json:"isactive"`
+	Rows     int               `json:"rows"`
+	Cols     int               `json:"cols"`
+	Ratio    float64           `json:"ratio"`
+	Style    map[string]string `json:"style"`
+	Widgets  []Widget          `json:"widgets"`
+	Grid     []GridItem        `json:"grid"`
 }
 
 func New(owner string) *Media {
 	return &Media{
-		Owner:       owner,
-		Rows:        10,
-		Cols:        10,
-		ScreenRatio: 16.0 / 9.0,
-		DisplayGrid: true,
-		Stylesvar:   make(map[string]interface{}),
-		Widgets:     []Widget{},
+		Owner:   owner,
+		Rows:    10,
+		Cols:    10,
+		Ratio:   16.0 / 9.0,
+		Style:   make(map[string]string),
+		Widgets: []Widget{},
 	}
 }
 
 type Widget struct {
-	PluginID   string                 `json:"pluginId"`
-	WidgetName string                 `json:"widgetName"`
-	Short      string                 `json:"short"`
-	Long       string                 `json:"long"`
-	X          int                    `json:"x"`
-	Y          int                    `json:"y"`
-	Rows       int                    `json:"rows"`
-	Cols       int                    `json:"cols"`
-	Props      map[string]interface{} `json:"props"`
+	ID            string                 `json:"id"`
+	PluginID      string                 `json:"pluginId"`
+	PluginVersion string                 `json:"pluginVersion"`
+	WidgetName    string                 `json:"widgetName"`
+	Short         string                 `json:"short"`
+	Long          string                 `json:"long"`
+	Props         map[string]interface{} `json:"props"`
+}
+
+type GridItem struct {
+	WidgetID string `widgetId`
+	X        int    `json:"x"`
+	Y        int    `json:"y"`
+	Rows     int    `json:"rows"`
+	Cols     int    `json:"cols"`
 }
--- api/db/plugins/plugin.go
+++ api/db/plugins/plugin.go
@@ -7,13 +7,14 @@ import (
 )
 
 type Plugin struct {
-	ID       string             `json:"id"`
-	Path     string             `json:"path" boltholdIndex:"Path"`
-	URL      string             `json:"url"`
-	Versions map[string]Version `json:"versions"`
+	ID       string    `json:"id"`
+	Path     string    `json:"path" boltholdIndex:"Path"`
+	URL      string    `json:"url"`
+	Versions []Version `json:"versions"`
 }
 
 type Version struct {
+	Version string   `json:"version"`
 	Short   string   `json:"short"`
 	Long    string   `json:"long"`
 	Widgets []Widget `json:"widgets"`
@@ -23,17 +24,15 @@ type Widget struct {
 	Name  string `json:"name"`
 	Short string `json:"short"`
 	Long  string `json:"long"`
-	Cols  int    `json:"cols"` // FIXME Are these defaults cols/rows? min? max?
-	Rows  int    `json:"rows"`
 	Props []Prop `json:"props"`
 }
 
 type Prop struct {
-	Name  string `json:"name"`
-	Short string `json:"short"`
-	Long  string `json:"long"`
-	Type  string `json:"type"`
-	Value string `json:"value"` // FIXME is this the default value?
+	Name         string `json:"name"`
+	Short        string `json:"short"`
+	Long         string `json:"long"`
+	Type         string `json:"type"`
+	DefaultValue string `json:"value"`
 }
 
 // GetDirectory returns the plugin's static files directory path
