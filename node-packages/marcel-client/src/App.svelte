88d98dd0b4f73b1d0aae66831fb08f720e021985
Valentin Cocaud <v.cocaud@gmail.com>
2020-01-26 17:22:07 +0100

    :recycle: use marcel-media in marcel-client

--- node-packages/marcel-client/src/App.svelte
+++ node-packages/marcel-client/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import "bulma/css/bulma.css";
+  import Media from "marcel-media";
   import { Message } from "svelma";
   import machine from "./machine";
   import AuthForm from "./components/auth-form.svelte";
@@ -18,20 +19,16 @@
   }
 </style>
 
-<link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css" />
-
 <div class="container">
   <div class="content">
     {#if $machine.state === 'loggedOut'}
       <AuthForm />
     {:else if $machine.state === 'noMedia'}
-      <Message active title="jjjjj">
+      <Message active title="Attention">
         Aucun media n'est actuellement associé
       </Message>
     {:else if $machine.state === 'mediaLoaded'}
-      <p>Le media !</p>
+      <Media media={$machine.context.media} />
     {:else if $machine.state === 'fatalError'}
       <Message active title="Error !">{$machine.context.error}</Message>
     {/if}
