2b49c5735f767d10c7141a32c562d55f14c8d770
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-02-05 00:43:51 +0100

    ♻️ noembed mode doesn't use disk for frontend and backoffice

--- backoffice/backoffice_archive.go
+++ backoffice/backoffice_archive.go
@@ -5,21 +5,11 @@ package backoffice
 import (
 	"fmt"
 	"io/fs"
-	"os"
-	"path/filepath"
 
-	"github.com/allez-chauffe/marcel/config"
 	"github.com/allez-chauffe/marcel/httputil"
 	"github.com/allez-chauffe/marcel/version"
 )
 
 func initFs() (fs.FS, error) {
-	url := fmt.Sprintf("https://github.com/allez-chauffe/marcel/releases/download/%[1]s/marcel-backoffice-%[1]s.tgz", version.Version())
-	path := filepath.Join(config.Default().API().DataDir(), "backoffice", version.Version())
-
-	if err := httputil.DownloadTgzToDir(url, path); err != nil {
-		return nil, err
-	}
-
-	return os.DirFS(path), nil
+	return httputil.GetTgzFS(fmt.Sprintf("https://github.com/allez-chauffe/marcel/releases/download/%[1]s/marcel-backoffice-%[1]s.tgz", version.Version()))
 }
