f865f7b7683d66854b4761980de72ad1b0ef3f7b
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2021-02-09 17:57:44 +0100

    :sparkles: Add Lookup/Register on module context

--- module/context.go
+++ module/context.go
@@ -1,8 +1,32 @@
 package module
 
-// Context contains information about the execution context.
-type Context interface{}
+import (
+	"fmt"
+	"reflect"
+)
 
-type ctx struct{}
+// Context contains information about the execution context.
+type Context interface {
+	Register(v interface{})
+	Lookup(p interface{}) (interface{}, bool)
+}
+
+type ctx struct {
+	values map[reflect.Type]interface{}
+}
 
 var _ Context = new(ctx)
+
+func (ctx *ctx) Register(v interface{}) {
+	t := reflect.TypeOf(v)
+	if _, ok := ctx.values[t]; ok {
+		panic(fmt.Errorf("%s already registered in modules context", t))
+	}
+	ctx.values[t] = v
+}
+
+func (ctx *ctx) Lookup(p interface{}) (interface{}, bool) {
+	t := reflect.TypeOf(p)
+	v, ok := ctx.values[t]
+	return v, ok
+}
